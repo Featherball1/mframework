cfa858256f0da24415991d83692b480b6eef91e2
Maria Cann <mariacann2003@gmail.com>
2025-10-18 17:34:46 +0100

    Scaffold the functional api

---
+++ mframework/__init__.py
@@ -0,0 +1,19 @@
+from mframework.tensor import Tensor, Parameter
+from mframework.functional import *
+
+__all__ = [
+    "Tensor",
+    "Parameter",
+    "add",
+    "sub",
+    "mul",
+    "div",
+    "matmul",
+    "neg",
+    "sum",
+    "exp",
+    "log",
+    "relu",
+    "transpose",
+    "reshape",
+]
---
+++ mframework/functional.py
@@ -0,0 +1,53 @@
+from typing import Tuple
+from mframework.tensor import Tensor
+
+"""
+There are two ways to call ops in mframework:
+    - Via Tensor methods, e.g., a + b, a.exp(), a.sum()
+    - Via functional API, e.g., mframework.add(a, b), mframeworkexp(a)
+This file implements the functional API by calling the corresponding Tensor methods.
+The functional API is then exposed in mframework/__init__.py to the end user. 
+"""
+
+# Arithmetic ops
+def add(a: Tensor, b: Tensor) -> Tensor:
+    return a + b
+
+def sub(a: Tensor, b: Tensor) -> Tensor:
+    return a - b
+
+def mul(a: Tensor, b: Tensor) -> Tensor:
+    return a * b
+
+def div(a: Tensor, b: Tensor) -> Tensor:
+    return a / b
+
+def matmul(a: Tensor, b: Tensor) -> Tensor:
+    return a @ b
+
+def neg(a: Tensor) -> Tensor:
+    return -a
+
+
+# Reduction ops
+def sum(a: Tensor, axis: int | None = None, keepdims: bool = False) -> Tensor:
+    return a.sum(axis=axis, keepdims=keepdims)
+
+
+# Function ops
+def exp(a: Tensor) -> Tensor:
+    return a.exp()
+
+def log(a: Tensor) -> Tensor:
+    return a.log()
+
+def relu(a: Tensor) -> Tensor:
+    return a.relu()
+
+
+# Shape ops
+def transpose(a: Tensor) -> Tensor:
+    return a.transpose()
+
+def reshape(a: Tensor, newshape: Tuple[int, ...]) -> Tensor:
+    return a.reshape(newshape)
